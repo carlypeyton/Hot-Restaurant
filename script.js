37e99120be597da9ed1b8843b8be5d4a8c97b3eb
Carly Peyton <carlypeyton@gmail.com>
2021-02-17 19:38:16 -0500

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+//add a table
+//clear all tables
+//get reservation list
+//get waitlist list
+
+
+//make reservation - post reservation data to server
+//view tables - get request from server / get waitlist
